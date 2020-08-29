be2bd29d8f3cdad5a2dba6ac6e2327a991b5d609
chudopal <chudopal.00@mail.ru>
2020-08-29 18:38:35 +0300

    added psql and create superuser

--- click_map/settings.py
+++ click_map/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'click_map.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'click_map',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}   
 
 
 # Password validation
