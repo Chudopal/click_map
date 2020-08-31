5f3d8ee5f90bc3e303bc618fd222e658f6ee7cd5
chudopal <chudopal.00@mail.ru>
2020-08-31 16:41:07 +0300

    added gesign

--- map/urls.py
+++ map/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('view', views.view_map, name="view"),
-    path('log', views.click_log, name="log"),
+    path('log', views.PointerListView.as_view(), name="log"),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- map/views.py
+++ map/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Pointer
 from django.contrib.gis.geos import Point
 import json
+from django.views import generic
 
 
 def view_map(request):
@@ -10,32 +11,23 @@ def view_map(request):
     return render(request, "map/map.html")
 
 
-def click_log(request):
-    """This function accepts post and get requests for pointers. 
+class PointerListView(generic.ListView):
+    """This class accepts post and get requests for pointers. 
     It allows to display all pointers."""
+    
+    model = Pointer
+    paginate_by = 13
+    template_name = "map/log.html"
+    
+    def post(self, request, *argv, **kwarg):
+        """This is a handler for get queries"""
 
-    if request.method == "POST":
         latlng = json.loads(request.body)
         point = Point(
             float(latlng["lat"]),
             float(latlng["lng"]),
             srid=4326
         )
-        print(dir(point))
 
-        pointer = Pointer()
-        
-        print(dir(pointer.location))
-
-        pointer.location = point
-        print("HEREEEEEE")
-        print(pointer)
-        print("HEREEEEEE")
+        pointer = Pointer(location=point)
         pointer.save()
-        return 302
-    if request.method == "GET":
-        coords = Pointer.objects.all()
-        context = {
-            "coords":coords,
-        }    
-        return render(request, "map/log.html", context)
